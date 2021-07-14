6b0da469e09aed17d500e53afe897394713f5ce7
Sudip Pokharel <sudieep661@gmail.com>
2021-07-14 17:15:02 +0545

    changes

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,5 +1,8 @@
 const menuIcon = document.getElementById('menu-icon'),
+  currentYear = document.getElementById('currentYear'),
   body = document.querySelector('body');
+
+currentYear.innerHTML = new Date().getFullYear();
 
 menuIcon.addEventListener('click', toggleMobileMenu);
 
